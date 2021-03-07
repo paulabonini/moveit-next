6eff3687f5f159a361fff78e048a2e43cb006662
Paula Bonini <paula_almeidabonini@hotmail.com>
2021-03-07 16:57:18 -0300

    tentando setar o login

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,28 +1,46 @@
+
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 import styles from '../styles/components/LoginBox.module.css';
 
 export function LoginBox() {
+
+  const { settingUser } = useContext(UserContext);
+
   return (
-    <div className={styles.loginBoxContainer}>
-      <div>
-        <img src="/icons/Logo.png" alt="Logo Move it" />
-      </div>
-      <div>
-        <header>
-          Bem-vindo
-        </header>
-        <main>
-          <img src="/icons/Git.png" alt="Logo Github" />
-        </main>
-        <footer>
-          <input type="text" />
-          <button
-            type="button"
-            className={styles.loginButton}
-          >
-            ->
-          </button>
-        </footer>
-      </div>
+    <div className={styles.container}>
+      <section>
+        <div className={styles.simbolo}>
+          <img src="/icons/Simbolo.png" alt="Simbolo" />
+        </div>
+        <div className={styles.loginBoxContainer}>
+          <div>
+            <img src="/icons/Logo.png" alt="Logo Move it" />
+          </div>
+          <div>
+            <header>
+              Bem-vindo
+            </header>
+            <main>
+              <img src="/icons/Git.png" alt="Logo Github" />
+            </main>
+            <footer>
+              <input type="text" placeholder="Digite seu username" />
+              <button
+                type="button"
+                name="user"
+                className={styles.loginButton}
+                onClick={settingUser}
+              >
+                -{'>'}
+              </button>
+            </footer>
+          </div>
+        </div>
+);
+    </section>
+
     </div>
-  );
+  )
+
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,80 @@
-import Login from './login';
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
-export default function Index() {
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { UserContext, UserContextProvider } from '../contexts/UserContext';
+
+import styles from '../styles/pages/Home.module.css';
+import { useContext, useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  isLogged: boolean;
+}
+
+export default function Home(props: HomeProps) {
+  const { isLogged, setIsLogged } = useContext(UserContext);
+
+  useEffect(function () {
+    setIsLogged(Boolean(Cookies.get('isLogged')));
+  }, [Cookies.get('isLogged')]);
+
   return (
-    <Login />
-  );
+    <UserContextProvider>
+
+      {isLogged && (
+        <ChallengesProvider
+          level={props.level}
+          currentExperience={props.currentExperience}
+          challengesCompleted={props.challengesCompleted}
+        >
+          <div className={styles.container}>
+            <Head>
+              <title>Início | move.it</title>
+            </Head>
+
+            <ExperienceBar />
+
+            <CountdownProvider>
+              <section>
+                <div>
+                  <Profile />
+                  <CompletedChallenges />
+                  <Countdown />
+                </div>
+                <div>
+                  <ChallengeBox />
+                </div>
+              </section>
+            </CountdownProvider>
+          </div>
+        </ChallengesProvider>
+      )}
+
+
+    </UserContextProvider>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted, isLogged } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+      isLogged: isLogged ? Boolean(isLogged) : false
+    }
+  }
 }
